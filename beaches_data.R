26b923fc5934e45657ac43ecac9bd74d1b5768e4
Kristy Robledo <kristy.robledo@sydney.edu,au>
2024-04-03 07:22:09 +1100

    add extra method

--- beaches_data.R
+++ beaches_data.R
@@ -1,7 +1,14 @@
-##Read in beaches data
+##Read in beaches data: 
+
+##option one, straight from github
 
 library (readr)
 
 urlfile="https://raw.githubusercontent.com/rladiessydney/RYouWithMe/master/sydneybeaches.csv"
 
 beaches<-read_csv(url(urlfile))
+
+##option two, via package
+
+library(ryouwithme)
+sydneybeaches <- sydneybeaches
